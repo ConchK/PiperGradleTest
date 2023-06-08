fe41974d63e51c40c5125db63e1ded333705bfb0
sora <eleutheros808@gmail.com>
2023-06-08 10:51:25 +0900

    first commit

---
+++ src/main/java/com/example/demo/PiperGradleTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiperGradleTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiperGradleTestApplication.class, args);
+	}
+
+}
